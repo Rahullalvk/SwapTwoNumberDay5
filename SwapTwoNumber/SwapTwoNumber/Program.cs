dd8349e22978784abd525be7fd9b81a4970d15d2
Rahulalvk <rahulal432@gmail.com>
2023-05-01 02:29:41 -0700

    Swap Two Number

---
+++ SwapTwoNumber/SwapTwoNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapTwoNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first Number Num1= ");
+            int Num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second Number Num2= ");
+            int Num2=Convert.ToInt32(Console.ReadLine());
+            Num1=Num1*Num2;
+            Num2 = Num1 / Num2;
+            Num1 = Num1 / Num2;
+            Console.WriteLine("Swapped Num1=" + Num1);
+            Console.WriteLine("Swapped Num2="+Num2);
+
+        }
+    }
+}
